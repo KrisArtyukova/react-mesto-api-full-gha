b4a63e4cead6a0282805ba44cfbae4b932e4a037
Kris Artyukova <kristinaartyukova@yandex.ru>
2023-09-19 22:10:45 +0300

    mesto-api-full v1.4
    Signed-off-by: ilyxi <prostokurtki@gmail.com>

--- backend/app.js
+++ backend/app.js
@@ -27,18 +27,52 @@ app.use(errorLogger); // подключаем логгер ошибок
 // обработчики ошибок
 app.use(errors()); // обработчик ошибок celebrate
 
+// Массив доменов, с которых разрешены кросс-доменные запросы
+const allowedCors = [
+  'http://krismesto.nomoredomainsrocks.ru/',
+  'https://krismesto.nomoredomainsrocks.ru/',
+  'localhost:3000'
+];
+
 app.use((err, req, res) => {
   // если у ошибки нет статуса, выставляем 500
   const { statusCode = 500, message } = err;
+  const { origin } = req.headers; // Сохраняем источник запроса в переменную origin
 
-  res
-    .status(statusCode)
-    .send({
-      // проверяем статус и выставляем сообщение в зависимости от него
-      message: statusCode === 500
-        ? 'На сервере произошла ошибка'
-        : message,
-    });
+  if (allowedCors.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+    const { method } = req; // Сохраняем тип запроса (HTTP-метод) в соответствующую переменную
+
+    // Значение для заголовка Access-Control-Allow-Methods по умолчанию (разрешены все типы запросов)
+    const DEFAULT_ALLOWED_METHODS = "GET,HEAD,PUT,PATCH,POST,DELETE";
+
+    // Если это предварительный запрос, добавляем нужные заголовки
+    if (method === 'OPTIONS') {
+      // разрешаем кросс-доменные запросы любых типов (по умолчанию)
+      res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+    }
+
+    // сохраняем список заголовков исходного запроса
+    const requestHeaders = req.headers['access-control-request-headers'];
+    if (method === 'OPTIONS') {
+      // разрешаем кросс-доменные запросы с этими заголовками
+      res.header('Access-Control-Allow-Headers', requestHeaders);
+      // завершаем обработку запроса и возвращаем результат клиенту
+      return res.end();
+    }
+
+    res
+      .status(statusCode)
+      .send({
+        // проверяем статус и выставляем сообщение в зависимости от него
+        message: statusCode === 500
+          ? 'На сервере произошла ошибка'
+          : message,
+      });
+
+  }
+
+
 });
 
 app.listen(PORT, () => {

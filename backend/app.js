b1482327bf8bff47d8de8928db7af68965b5f2de
Kris Artyukova <kristinaartyukova@yandex.ru>
2023-09-20 13:16:52 +0300

    mesto-api-full v1.8
    Signed-off-by: ilyxi <prostokurtki@gmail.com>

--- backend/app.js
+++ backend/app.js
@@ -13,7 +13,7 @@ const app = express();
 mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
 });
-app.use(cors());
+app.use(cors);
 app.use(bodyParser.json()); // для собирания JSON-формата
 app.use(bodyParser.urlencoded({ extended: true })); // для приёма веб-страниц внутри POST-запроса
 app.use(requestLogger); // подключаем логгер запросов
